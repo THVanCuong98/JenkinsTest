66ef8d29c441abf06d437cc78eafabdd47474f17
Voldemort <vankien.it3.hut@gmail.com>
2019-02-16 10:51:20 +0700

    update

--- src/main/java/com/example/demo/TestJenkinsApplication.java
+++ src/main/java/com/example/demo/TestJenkinsApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-public class TestJenkinsApplication {
+public class TestJenkinsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestJenkinsApplication.class, args);
--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@ComponentScan
 @RestController
 public class MainController {
 	
